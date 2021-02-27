b6d8093de72f9f8605fdb0273561e0bf42b418e0
EvgeniyMay <yevgeniy.may@gmail.com>
2021-02-27 15:35:27 +0200

    add project structure

---
+++ src/main/java/com/may/soulrecipes/SoulRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.may.soulrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoulRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/may/soulrecipes/SoulRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.may.soulrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
